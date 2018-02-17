2984145421b7e64bbfa923fe63e8c9a0f03c0d38
CMDR Moraes <14825927+wesleymoura@users.noreply.github.com>
2018-02-16 23:49:30 -0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -16,13 +16,13 @@ config.text = {
 	mainbackgroundone: "http://cdn.escapistmagazine.com/media/global/images/library/deriv/632/632377.jpg",
 	mainbackgroundtwo: "http://www.mytrickrc.com/wp-content/uploads/2013/11/Mustang_L34_dark.jpg",
 	mainbackgroundthree: "https://img.gta5-mods.com/q75/images/mastercom-b-siren/02f0b4-AFPGetty-167087672.jpg",
-	maintextone: "Text to be displayed on the first image of the slideshow on the home page",
-	maintexttwo: "Text to be displayed on the second image of the slideshow on the home page",
-	maintextthree: "Text to be displayed on the third image of the slideshow on the home page",
-	sherifflogo: "URL to sheriff logo",
-	pdlogo: "URL to police dept. logo",
-	emslogo: "URL to fire rescue logo",
-	highwaylogo: "URL to highway patrol logo"
+	maintextone: "Para Servir e Proteger",
+	maintexttwo: "Respeito acima de tudo",
+	maintextthree: "Ate o dia em wie for chamado por DEUS",
+	sherifflogo: "https://i.imgur.com/VGHGmAn.jpg",
+	pdlogo: "https://i.imgur.com/YUWyKbx.jpg",
+	emslogo: "https://i.imgur.com/XxkqH8K.png",
+	highwaylogo: "https://i.imgur.com/ShWuZQW.jpg"
 
 
 }
